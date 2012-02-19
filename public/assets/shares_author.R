58e7c3627a1aa1f6ee0a676b99376c275cbdfeb4
Martin Fenner <fenner.martin@mh-hannover.de>
2012-02-19 20:57:32 +0000

    First working version of R script of author classification

--- public/assets/shares_author.R
+++ public/assets/shares_author.R
@@ -3,23 +3,29 @@
 # by Martin Fenner <fenner.martin@mh-hannover.de>
 
 # Import crowdometer result set from figshare
-# tweets <- read.csv("http://figshare.com/media/download/97336/89546", sep=",")
+#tweets <- read.csv("http://figshare.com/media/download/97336/89546", sep=",")
 classifications <- read.csv("http://figshare.com/media/download/97336/89547", sep=",")
 
-author.freq <- table(classifications$shares_author)
+# Combine classifications with tweets, using max value. This will return true
+# if at least 50% of classifications are true
+results <- unstack(classifications, form=shares_author~tweet_id)
+results <- lapply(results, max)
+
+# Get a vector of the results, turn into table that counts all results
+results <- table(unlist(results, use.names=FALSE))
 
 # Subtitle
-piesubtitle <- sprintf("%3.0f Tweets", sum(author.freq))
+piesubtitle <- sprintf("%3.0f Tweets", sum(results))
 
 # Create label
-pielabels <- sprintf("%s (%2.0f%s)", names(author.freq), 100*author.freq/sum(author.freq), "%")
+pielabels <- sprintf("%s (%2.0f%s)", names(results), 100*results/sum(results), "%")
 
 # Load color palette
 library(RColorBrewer)
-colors = brewer.pal(7, "Blues")
+colors = brewer.pal(3, "Blues")
 
 # Do the plotting
 opar <- par(mar=c(4,2,2,2), cex.sub=0.8, cex.main=1.4, fg="black", col.main="#25679a")
-pie(author.freq, labels=pielabels, col=colors, init.angle=130, cex=0.8, lty=1)
+pie(results, labels=pielabels, col=colors, init.angle=130, cex=0.8, lty=1)
 title(main="Tweets by Author/Publisher of Paper", sub=piesubtitle, line=-1)
 par(opar)
